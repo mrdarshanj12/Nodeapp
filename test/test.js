e28a5354545364c7ef5e60b9c03226b14fbba9f1
ravi bagale <ravi2.bagale@gmail.com>
2023-02-23 18:38:32 +0530

    test repaired

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 var request = require('supertest');
-var app = require('../index.js');
-describe('GET /', function() {
+var app = require('../index.js');describe('GET /', function() {
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
  request(app).get('/').expect('hello world', done);
  });
