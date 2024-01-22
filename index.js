e8c768a5da8eca0055642d2af3d465c98c764a25
ravibagale <30994679+ravibagale@users.noreply.github.com>
2024-01-22 17:00:30 +0530

    agar tum

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('ye ruh bhi mera');
+ res.send('agar tum saath ho');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
