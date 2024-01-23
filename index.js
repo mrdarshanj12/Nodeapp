f1e3c9a12112ba227abc23455e5cbbacb9ed2754
ravibagale <30994679+ravibagale@users.noreply.github.com>
2024-01-23 13:57:11 +0530

    kuch kr

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('kahaani bu adhuri hai abhi');
+ res.send('kr raha hu per ho nahi raha');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
